7bcb59ba4a4d2854a883407b1bd0f689e83e30a8
Peter Sivilay <sivilaypeter@gmail.com>
2017-02-06 22:23:38 -0600

    Pass 2nd spec to see if includes a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 //business logic back end
 
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-
 var pigLatin = function(userInput) {
-  if (userInput / 1 === userInput) {
-    return userInput;
+  if (isNaN(userInput) === false) {
+    return userInput
+  } else if (userInput.includes("i")) {
+    return userInput + "ay"
   }
 };
 
@@ -13,7 +13,7 @@ var pigLatin = function(userInput) {
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
-    var userInput = parseInt($("input#word").val());
+    var userInput = $("input#word").val();
 
     var result = pigLatin(userInput);
 
