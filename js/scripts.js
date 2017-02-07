47c96a7e19871040b456091fc2e858a1057f5b42
Peter Sivilay <sivilaypeter@gmail.com>
2017-02-06 21:49:41 -0600

    Pass 1st spec of non-alphabet characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 //business logic back end
 
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
+var pigLatin = function(userInput) {
+  if (userInput / 1 === userInput) {
+    return userInput;
+  }
+};
 
 //user logic front end
 
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
-    var userInput = $("input#word").val();
+    var userInput = parseInt($("input#word").val());
 
+    var result = pigLatin(userInput);
 
-    $("#result").text(userInput).show();
-    //$("#result").show();
+    $("#result").text(result).show();
   });
 });
